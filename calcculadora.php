3d30acb84e940ba594796fcc8c7661cf3cd188f7
Pablo Valerio <22040010@alumno.utc.edu.mx>
2024-09-26 18:07:27 -0600

    NO JALA ESTE PDO

--- calcculadora.php
+++ calcculadora.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tabla de sumar</title>
+    <title> de sumar</title>
 </head>
 <body>
 <form method="post">
